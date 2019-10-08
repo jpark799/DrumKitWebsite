eae70d2a36eec06f3a52c5f7d07dfe6dd4dfc366
jpark799 <jason.j.park79@gmail.com>
2019-10-08 17:10:30 +0900

    WIP: switchstatements need to be updated

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-document.querySelector("button").addEventListener("click",handleClick)
+const numberOfDrumButtons = document.querySelectorAll(".drum").length
+const audio = new Audio('./sounds/kick-bass.mp3')
 
-function handleClick() {
-    alert("I got clicked")
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click",function () {
+        let buttonInnerHTML = this.innerHTML
+
+        switch (buttonInnerHTML) {
+            case 'w':
+                alert('w was just clicked')          
+                break;
+        
+            default:
+                break;
+        }
+    })
 }
+
+//     function handleClick() {
+//         audio.play();
+//     }
+// }
