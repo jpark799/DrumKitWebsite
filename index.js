e8703daa559316fd1f12b21d2f0b795942ac31db
jpark799 <jason.j.park79@gmail.com>
2019-10-09 23:35:32 +0900

    changed variables from lets to consts

--- index.js
+++ index.js
@@ -9,37 +9,37 @@ function handleClick() {
 
   switch (buttonInnerHTML) {
     case "w":
-            let crash = new Audio("./sounds/crash.mp3")
+            const crash = new Audio("./sounds/crash.mp3")
             crash.play()
         break
 
     case "a":
-            let kickBass = new Audio("./sounds/kick-bass.mp3")
+            const kickBass = new Audio("./sounds/kick-bass.mp3")
             kickBass.play()
         break
 
     case "s":
-            let snare = new Audio("./sounds/snare.mp3")
+            const snare = new Audio("./sounds/snare.mp3")
             snare.play()
         break
 
     case "d":
-            let tom1 = new Audio("./sounds/tom1.mp3")
+            const tom1 = new Audio("./sounds/tom1.mp3")
             tom1.play()
         break
 
     case "j":
-            let tom2 = new Audio("./sounds/tom2.mp3")
+            const tom2 = new Audio("./sounds/tom2.mp3")
             tom2.play()
         break
 
     case "k":
-            let tom3 = new Audio("./sounds/tom3.mp3")
+            const tom3 = new Audio("./sounds/tom3.mp3")
             tom3.play()
         break
 
     case "l":
-            let tom4 = new Audio("./sounds/tom4.mp3")
+            const tom4 = new Audio("./sounds/tom4.mp3")
             tom4.play()
         break
 
