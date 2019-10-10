67ec1ec7c6ddbf11d57a2db2c28dd7135cf29859
jpark799 <jason.j.park79@gmail.com>
2019-10-10 16:31:06 +0900

    accidently removed semicolons in for loop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const numberOfDrumButtons = document.querySelectorAll(".drum").length
 
-for (let i = 0 i < numberOfDrumButtons i++) {
+for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", handleClick)
 }
 
