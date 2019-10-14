a6d05d3f0a6a8cf26b707c7b05885d9b405c09f5
jpark799 <jason.j.park79@gmail.com>
2019-10-14 17:42:52 +0900

    logging key pressed

--- index.js
+++ index.js
@@ -49,52 +49,11 @@ function handleClick() {
   }
 }
 
-for (let i = 0; i < numberOfDrumButtons; i++) {
-  document
-    .querySelectorAll("button")
-    [i].addEventListener("keydown", handleKeypress);
-}
+const log = document.getElementById('log')
 
-function handleKeypress() {
-  let buttonInnerHTML = this.innerHTML;
+document.addEventListener('keypress', logKey)
 
-  switch (buttonInnerHTML) {
-    case "w":
-      const crash = new Audio("./sounds/crash.mp3");
-      crash.play();
-      break;
-
-    case "a":
-      const kickBass = new Audio("./sounds/kick-bass.mp3");
-      kickBass.play();
-      break;
-
-    case "s":
-      const snare = new Audio("./sounds/snare.mp3");
-      snare.play();
-      break;
-
-    case "d":
-      const tom1 = new Audio("./sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-    case "j":
-      const tom2 = new Audio("./sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "k":
-      const tom3 = new Audio("./sounds/tom-3.mp3");
-      tom3.play();
-      break;
-
-    case "l":
-      const tom4 = new Audio("./sounds/tom-4.mp3");
-      tom4.play();
-      break;
-
-    default:
-      break;
-  }
+function logKey(e) {
+  let keyPressed = e.code
+  console.log(keyPressed)
 }
