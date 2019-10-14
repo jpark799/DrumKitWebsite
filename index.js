8d0786ce6827aa71dbe7fd8cc523d3e57705aa8a
jpark799 <jason.j.park79@gmail.com>
2019-10-14 17:47:39 +0900

    sound playing with keyboard clicks

--- index.js
+++ index.js
@@ -55,5 +55,45 @@ document.addEventListener('keypress', logKey)
 
 function logKey(e) {
   let keyPressed = e.code
-  console.log(keyPressed)
+
+  switch (keyPressed) {
+    case "KeyW":
+        const crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+      break;
+  
+    case "KeyA":
+        const kickBass = new Audio("./sounds/kick-bass.mp3");
+        kickBass.play();
+      break;
+  
+    case "KeyS":
+        const snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+    break;
+  
+    case "KeyD":
+        const tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+      break;
+  
+    case "KeyJ":
+        const tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+      break;
+  
+    case "KeyK":
+        const tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+      break;
+  
+    case "KeyL":
+        const tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+      break;
+  
+    default:
+        console.log(`${keyPressed}`)
+      break;
+  }
 }
